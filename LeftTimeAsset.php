db07dc44d4eeab132ab4a0574e2b737e67e63d1d
unknown <github@web-studio.cv.ua>
2017-07-06 21:39:36 +0300

    asset->vendor

--- LeftTimeAsset.php
+++ LeftTimeAsset.php
@@ -5,7 +5,7 @@ use yii\web\AssetBundle;
 
 class LeftTimeAsset extends AssetBundle
 {
-    public $sourcePath = '@ws-cv-ua/left-time';
+    public $sourcePath = '@vendor/ws-cv-ua/left-time';
 
     public $js = [
         'js/left-time.js'
